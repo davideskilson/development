72df8403eea12a8769eae5863c734aa7dbc09ff1
davideskilson <david_eskilson@brown.edu>
2024-04-04 17:38:18 -0400

    feat: acessibility alt text issueu addressed

--- my-app/src/components/BookItem.js
+++ my-app/src/components/BookItem.js
@@ -1,7 +1,7 @@
 import "./BookItem.css";
 
 export default function BookItem(props) {
-  const alt = "Image of the book" + props.item.title;
+  const alt = "The book" + props.item.title;
   let isFavorite;
   if (props.favorites) {
     isFavorite = props.favorites.includes(props.item.title);
